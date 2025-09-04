deab33ee959ae9507508056f840f356bc3e67637
kronnoss <kronnoss.it@gmail.com>
2025-09-05 01:02:13 +0300

    Update file controller.js

--- src/appStates/controller.js
+++ src/appStates/controller.js
@@ -1,33 +1,39 @@
 import * as yup from 'yup'
 
-const schema = yup.object().shape({
-  // url??
-  url: yup.string().required().url(),
+// setLocale (без вызова i18n.t()a)
+
+yup.setLocale({
+  mixed: {
+    required: () => 'errors.validation.required',
+    notOneOf: () => 'errors.validation.duplicated',
+  },
+  string: {
+    url: () => 'errors.validation.incorrecredUrl',
+  },
 })
 
-const feeds = [
-  {
-    url: 'invalid-url',
-  },
-  {
-    url: 'https://www.google.com/',
-  },
-]
+const validateUrl = (currentUrl, feeds) => {
+  const schema = yup.object().shape({
+    // url??
+    url: yup.string().required().url().notOneOf(feeds),
+  })
 
-// validateUrl
-// validateCopy
+  schema
+    .validate(currentUrl)
+    .then((data) => {
+      console.log('valid url', data.url)
+    })
+    .catch((error) => {
+      // console.log('error', error)
+      console.log('message', error.message)
+    })
+}
 
 export default (watchedState) => {
   console.log(watchedState)
-  feeds.forEach((feed) => {
-    schema
-      .validate(feed)
-      .then((data) => {
-        console.log('valid url', data.url)
-      })
-      .catch((error) => {
-        console.log('error', error)
-        console.log('message', error.message)
-      })
-  })
+  const urlValue = {
+    url: 'invalid-url',
+  }
+  const feeds = ['invalid-url', 'https://www.google.com/', '']
+  validateUrl(urlValue, feeds)
 }
