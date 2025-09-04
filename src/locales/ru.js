b06f9cb86ad284099806364056dc132af4298d54
kronnoss <kronnoss.it@gmail.com>
2025-09-05 01:00:38 +0300

    Add locales for i18next

---
+++ src/locales/ru.js
@@ -0,0 +1,18 @@
+export default {
+  translation: {
+    app: {
+      title: 'RSS агрегатор',
+      subtitle: 'Начните читать RSS сегодня! Это легко, это красиво.',
+      formPlaceholder: 'Ссылка RSS',
+      submitButton: 'Добавить',
+      exampleRSS: 'Пример: https://lorem-rss.hexlet.app/feed',
+    },
+    errors: {
+      validation: {
+        required: 'Данное поле являестя обязательным...',
+        duplicated: 'Введенный RSS-адрес уже существует',
+        incorrecredUrl: 'Введенный RSS-адрес должен быть валидным Url',
+      },
+    },
+  },
+}
