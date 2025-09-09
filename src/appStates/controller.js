d72205d7a042e265fa8690a7e99b452e8bff0e1c
kronnoss <kronnoss.it@gmail.com>
2025-09-10 00:06:53 +0300

    Update controller.js, add parser and request logic

--- src/appStates/controller.js
+++ src/appStates/controller.js
@@ -1,38 +1,94 @@
 import * as yup from 'yup'
+import axios from 'axios'
+import uniqueId from 'lodash.uniqueid'
+
+const feedbackInners = {
+  required: 'feedback.validation.required',
+  duplicated: 'feedback.validation.duplicated',
+  incorrecredUrl: 'feedback.validation.incorrecredUrl',
+  fail: 'feedback.processStates.fail',
+  success: 'feedback.processStates.success',
+  network: 'feedback.network',
+}
 
 yup.setLocale({
   mixed: {
-    required: () => 'errors.validation.required',
-    notOneOf: () => 'errors.validation.duplicated',
+    required: () => feedbackInners.required,
+    notOneOf: () => feedbackInners.duplicated,
   },
   string: {
-    url: () => 'errors.validation.incorrecredUrl',
+    url: () => feedbackInners.incorrecredUrl,
   },
 })
 
-const initSchema = feeds => yup.object().shape({
-  url: yup.string().required().url().notOneOf(feeds),
-})
+const initSchema = urls =>
+  yup.object().shape({
+    url: yup.string().required().url().notOneOf(urls),
+  })
+
+const routes = url => `https://allorigins.hexlet.app/get?disableCache=true&url=${url}`
+
+const parseXMLString = (str) => {
+  const parser = new DOMParser()
+  const doc = parser.parseFromString(str, 'application/xml')
+  // commit 'change logic with feedback element' ??
+  const isErrorFormat = doc.querySelector('parsererror')
+  const isRSSFormat = doc.querySelector('rss')
+  if (isErrorFormat && !isRSSFormat) throw new Error(feedbackInners.fail)
+
+  const feedId = uniqueId()
+  const data = {
+    feed: {
+      id: feedId,
+      title: doc.querySelector('title').textContent,
+      description: doc.querySelector('description').textContent,
+    },
+    posts: [],
+  }
+
+  const posts = doc.querySelectorAll('item')
+  data.posts = [...posts].map(post => ({
+    feedId,
+    title: post.querySelector('title').textContent,
+    description: post.querySelector('description').textContent,
+    link: post.querySelector('link').textContent,
+  }))
+  return data
+}
 
 export default (watchedState, elements) => {
-  const { formElement, inputElement } = elements
-  inputElement.focus() // autofocus??
+  const { formElement } = elements
+  watchedState.processStatus = 'default'
 
   formElement.addEventListener('submit', (event) => {
     event.preventDefault()
-    const { feeds } = watchedState.form
+    const { urls } = watchedState.form
     const formData = new FormData(formElement)
     const newUrl = Object.fromEntries(formData)
 
-    const schema = initSchema(feeds)
+    const schema = initSchema(urls)
     schema
       .validate(newUrl)
       .then(() => {
-        watchedState.form.error = ''
-        feeds.push(newUrl.url)
+        // watchedState.feedback = '' // ??
+        const rightUrl = routes(newUrl.url)
+        watchedState.processStatus = 'sending'
+        return axios.get(rightUrl)
+      })
+      .then((response) => {
+        const xmlString = response.data.contents
+        const parsingData = parseXMLString(xmlString)
+
+        watchedState.processStatus = 'success'
+        watchedState.feedback = feedbackInners.success
+        urls.push(newUrl.url)
+        watchedState.fields.feeds.push(parsingData.feed)
+        watchedState.fields.posts = [...watchedState.fields.posts, ...parsingData.posts] // корректно ли??
+        console.log('watchedState.fields', watchedState.fields)
       })
       .catch((error) => {
-        watchedState.form.error = error.message
+        watchedState.feedback = error.message === 'Network Error' ? feedbackInners.network : error.message
+        watchedState.processStatus = 'fail'
       })
   })
 }
